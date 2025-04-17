15910b5b01bca794e0fa5eeefcf003bbcf5ab454
Bibekananda Jana <mbibekjana@gmail.com>
2025-04-17 16:09:45 +0530

    Update bedrock_agent_runtime.py

--- services/bedrock_agent_runtime.py
+++ services/bedrock_agent_runtime.py
@@ -11,7 +11,7 @@ def invoke_agent(agent_id, agent_alias_id, session_id, prompt):
         AWS_SECRET_ACCESS_KEY = st.secrets["AWS_SECRET_ACCESS_KEY"]
         AWS_REGION = st.secrets.get("AWS_REGION", "us-east-1")
         REGION = AWS_REGION
-        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", REGION,AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
+        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", REGION = AWS_REGION)
         # See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html
         response = client.invoke_agent(
             agentId=agent_id,
