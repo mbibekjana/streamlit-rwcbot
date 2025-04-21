cf9cdd608003b65b5a8f9eb98dfc9ac5a03c19e2
Bibekananda Jana <mbibekjana@gmail.com>
2025-04-21 14:35:21 +0530

    Update bedrock_agent_runtime.py

--- services/bedrock_agent_runtime.py
+++ services/bedrock_agent_runtime.py
@@ -37,19 +37,19 @@ def invoke_agent(agent_id, agent_alias_id, session_id, prompt):
                     citations += chunk["attribution"]["citations"]
 
             # Extract trace information from all events
-            if "trace" in event:
-                for trace_type in ["guardrailTrace", "preProcessingTrace", "orchestrationTrace", "postProcessingTrace"]:
-                    if trace_type in event["trace"]["trace"]:
-                        mapped_trace_type = trace_type
-                        if trace_type == "guardrailTrace":
-                            if not has_guardrail_trace:
-                                has_guardrail_trace = True
-                                mapped_trace_type = "preGuardrailTrace"
-                            else:
-                                mapped_trace_type = "postGuardrailTrace"
-                        if trace_type not in trace:
-                            trace[mapped_trace_type] = []
-                        trace[mapped_trace_type].append(event["trace"]["trace"][trace_type])
+            # if "trace" in event:
+            #     for trace_type in ["guardrailTrace", "preProcessingTrace", "orchestrationTrace", "postProcessingTrace"]:
+            #         if trace_type in event["trace"]["trace"]:
+            #             mapped_trace_type = trace_type
+            #             if trace_type == "guardrailTrace":
+            #                 if not has_guardrail_trace:
+            #                     has_guardrail_trace = True
+            #                     mapped_trace_type = "preGuardrailTrace"
+            #                 else:
+            #                     mapped_trace_type = "postGuardrailTrace"
+            #             if trace_type not in trace:
+            #                 trace[mapped_trace_type] = []
+            #             trace[mapped_trace_type].append(event["trace"]["trace"][trace_type])
 
     except ClientError as e:
         raise
